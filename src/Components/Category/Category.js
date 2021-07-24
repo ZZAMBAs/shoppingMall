16f3c07160e767e57034c12316fbd2d58f289005
ZZAMBAs <rksidksrksi@naver.com>
2021-07-24 22:59:55 +0900

    Create category page(Category.css, Category.js).

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,9 +1,19 @@
 import React from "react";
+import cate1 from "../../images/1.jpg"
+import cate2 from "../../images/test.jpg"
+import ImgBox from "./ImgBox";
+import './Category.css'
 
 function Category() {
+
+    const cateName = ["카테고리1", "카테고리2","카테고리3", "카테고리4","카테고리5", "카테고리6","카테고리7", "카테고리8","카테고리9"];
+    const cateSrc = [cate1, cate2, cate1, cate2, cate1, cate2, cate1, cate2, cate1];
+    const cateObj = cateName.map((mem, idx) => {return {name: mem, src: cateSrc[idx]}});
+    // 카테고리 이름, 카테고리 이미지 소스(주소)로 구성된 객체
+
   return (
-    <div>
-      <h1>카테고리</h1>
+    <div className="cate_all">
+        <ImgBox cate={cateObj} />
     </div>
   );
 }
